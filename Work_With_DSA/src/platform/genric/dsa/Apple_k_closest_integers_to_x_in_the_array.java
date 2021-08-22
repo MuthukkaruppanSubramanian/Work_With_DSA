18584b61a91f0a1e446055047d937dd38b22a4f8
Muthukaruppan <37132906+MuthukkaruppanSubramanian@users.noreply.github.com>
2021-08-22 06:35:25 +0530

    Apple Interview Questions

---
+++ Work_With_DSA/src/platform/genric/dsa/Apple_k_closest_integers_to_x_in_the_array.java
@@ -0,0 +1,53 @@
+import org.junit.*;
+import java.util.*;
+
+public class Apple_k_closest_integers_to_x_in_the_array {
+
+//	Question 2:
+//		Given a sorted integer array arr, two integers k and x, return the k closest integers to x in the array. The result should also be sorted in ascending order.
+//		An integer a is closer to x than an integer b if:
+//		|a - x| < |b - x|, or
+//		|a - x| == |b - x| and a < b
+//		 
+//		Example 1:
+//		Input: arr = [1,2,3,4,5], k = 4, x = 3
+//		Output: [1,2,3,4]
+//		Example 2:
+//		Input: arr = [1,2,3,4,5], k = 4, x = -1
+//		Output: [1,2,3,4]
+//		 Example 3:
+//		Input: arr = [8,17,24,25,28], k = 3, x = 23
+//		Output: [24,25,28]
+//		2 
+//		Output -> List<Integer>
+	/*
+	 * Sample Test Data 
+	 *    Input : 
+	 *    output: 
+	 *
+	 * Approach 1: Test_Template.java
+	 * 
+	 *
+	 * Time / Space Complexity:
+	 * 		
+	 */
+	
+	@Test
+	public void test1() {
+
+	}
+
+	@Test
+	public void test2() {
+
+	}
+
+	@Test
+	public void test3() {
+
+	}
+	
+	/*
+	 * Pseudo Code
+	 */
+}
