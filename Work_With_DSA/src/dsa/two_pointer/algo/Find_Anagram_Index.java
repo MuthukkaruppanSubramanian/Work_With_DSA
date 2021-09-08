793cd15acf251d5776c81980e52b9d7c65ab7b09
Muthukaruppan <37132906+MuthukkaruppanSubramanian@users.noreply.github.com>
2021-09-08 21:43:12 +0530

    Add files via upload

---
+++ Work_With_DSA/src/dsa/two_pointer/algo/Find_Anagram_Index.java
@@ -0,0 +1,53 @@
+package dsa.contest.algo;
+import org.junit.*;
+import java.util.*;
+
+public class Find_Anagram_Index {
+	
+	/*
+	 * 
+	 *
+	 * Sample Test Data 
+	 *    Input : 
+	 *    output: 
+	 *
+	 * Approach 1: Test_Template.java
+	 * 
+	 *
+	 * Time / Space Complexity:
+	 * 		
+	 */
+	
+	@Test
+	public void test1() {
+
+	}
+
+	@Test
+	public void test2() {
+
+	}
+
+	@Test
+	public void test3() {
+
+	}
+	
+	/*
+	 * Pseudo Code
+	 * 	1. Declare a pattern map and add the k string to it with its occurrence
+	2. Declare a current map
+	3. Declare two pointer left and right starts at 0.
+	4. Traverse the string in a while loop until right reaches the length of the s
+	5. -----add the char and its occurrence to current map
+	6. -----if the right-left+1 == k.length
+	7. -----------if(currentMap equals pattern map)
+	8. -----------------store the left index to list
+	9. -----------------reduce the char count at left and do left++
+	10. ----right++
+		Return list.
+	 */
+	
+	
+	public List<Integer> 
+}
