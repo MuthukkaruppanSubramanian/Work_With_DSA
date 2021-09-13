92d8b790e06783265d37f6d7f8966b7427939c1c
Muthukaruppan <37132906+MuthukkaruppanSubramanian@users.noreply.github.com>
2021-09-13 18:11:22 +0530

    Add files via upload

---
+++ Work_With_DSA/src/dsa/two_pointer/algo/P_04_Sign_of_the_Product_of_an_Array_LC_1822.java
@@ -0,0 +1,54 @@
+package com.two_pointer.algo;
+import org.junit.*;
+import java.util.*;
+
+public class P_04_Sign_of_the_Product_of_an_Array_LC_1822 {
+	
+	/*
+	 * https://leetcode.com/problems/sign-of-the-product-of-an-array/
+	 *
+	 * Sample Test Data 
+	 *    Input : 
+	 *    output: 
+	 *
+	 * Approach 1: Test_Template.java
+	 * 
+	 *
+	 * Time / Space Complexity:
+	 * 		
+	 */
+	
+	@Test
+	public void test1() {
+		int[] nums = {1,1,-2,-2};
+		Assert.assertTrue(arraySign(nums) == 1);
+	}
+
+	@Test
+	public void test2() {
+		int[] nums = {1,1,0,-2};
+		Assert.assertTrue(arraySign(nums) == 0);
+	}
+
+	@Test
+	public void test3() {
+		int[] nums = {1,1,2,-2};
+		Assert.assertTrue(arraySign(nums) == -1);
+	}
+	
+	/*
+	 * Pseudo Code
+	 */
+	
+    public int arraySign(int[] nums) {
+        int left = 0, right = nums.length-1;
+        double product = 1;
+        for(int i = 0; i < nums.length; i++){
+            product *= (nums[i]);
+        }
+        
+        if(product == 0) return 0;
+        
+        return product > 0? 1 : -1;
+    }
+}
